bfbcb47ca96ba02b71363a463db2eca31890c4bf
unknown <ahmadzytoon@outlook.com>
2022-11-03 20:04:05 +0300

    Tasks

---
+++ javascrept/Thursday11-3/script3.js
@@ -0,0 +1,38 @@
+
+ x = prompt("Enter Tag");
+ parseInt(x);
+
+if (x<50) {
+    alert ("FAIL");
+    console.log('FAIL');
+
+} 
+else if (x>=50&&x<=59) {
+    alert ("D");
+    console.log('D');
+
+}
+else if(x>=60 && x<=69) {
+    alert ("C");
+    console.log('C');
+
+} 
+else if (x>=70 && x<=79) {
+    alert ("B");
+    console.log('B');
+
+} 
+else if (x>=80 && x<=89) {
+    alert ("A");
+    console.log('A');
+
+} 
+else if (90<= x <=100) {
+    alert ("A+");
+    console.log('A+');
+
+} 
+else { alert ("error");}
+
+
+
